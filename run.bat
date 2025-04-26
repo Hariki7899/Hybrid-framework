d6e926418703ec70554ca88f96d71b5c307bcfec
Hari <harivenkatakrishnan7899@gmail.com>
2025-04-26 09:31:22 +0530

    Updating bat file

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
-pytest -s -v -m "sanity" --html=./Reports/report.html TestCases/ --browser chrome
-REM pytest -s -v -m "sanity" --html=./Reports/report.html TestCases/ --browser firefox
-REM pytest -s -v -m "sanity" --html=./Reports/report.html TestCases/ 
-REM pytest -s -v -m "sanity or regression" --html=./Reports/report.html TestCases/ --browser chrome
-REM pytest -s -v -m "regression" --html=./Reports/report.html TestCases/ --browser chrome
-REM pytest -s -v -n=4 -m "sanity" --html=./Reports/report.html TestCases/ --browser chrome
-REM pytest -s -v -n=6 -m "sanity or regression" --html=./Reports/report.html TestCases/ --browser chrome
+"C:\Users\91638\PycharmProjects\Hybrid framework\.venv\Scripts\python.exe" -m pytest -s -v -m "sanity" --html=./Reports/report.html TestCases/ --browser chrome
+REM "C:\Users\91638\PycharmProjects\Hybrid framework\.venv\Scripts\python.exe" -m pytest -s -v -m "sanity" --html=./Reports/report.html TestCases/ --browser firefox
+REM "C:\Users\91638\PycharmProjects\Hybrid framework\.venv\Scripts\python.exe" -m pytest -s -v -m "sanity" --html=./Reports/report.html TestCases/
+REM "C:\Users\91638\PycharmProjects\Hybrid framework\.venv\Scripts\python.exe" -m pytest -s -v -m "sanity or regression" --html=./Reports/report.html TestCases/ --browser chrome
+REM "C:\Users\91638\PycharmProjects\Hybrid framework\.venv\Scripts\python.exe" -m pytest -s -v -m "regression" --html=./Reports/report.html TestCases/ --browser chrome
+REM "C:\Users\91638\PycharmProjects\Hybrid framework\.venv\Scripts\python.exe" -m pytest -s -v -n=4 -m "sanity" --html=./Reports/report.html TestCases/ --browser chrome
+REM "C:\Users\91638\PycharmProjects\Hybrid framework\.venv\Scripts\python.exe" -m pytest -s -v -n=6 -m "sanity or regression" --html=./Reports/report.html TestCases/ --browser chrome
